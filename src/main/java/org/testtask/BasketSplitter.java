bb53b98c7250127961f93495e9de01581dc2a44d
ZakharSytoi <zakharsytoi@gmail.com>
2024-04-07 15:13:28 +0200

    BasketSplitter split method implemented

--- src/main/java/org/testtask/BasketSplitter.java
+++ src/main/java/org/testtask/BasketSplitter.java
@@ -1,10 +1,11 @@
 package org.testtask;
 
+
+import org.testtask.exception.ProductNotExistsException;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BasketSplitter {
 
@@ -28,8 +29,46 @@ public class BasketSplitter {
     }
 
     public Map<String, List<String>> split(List<String> items) throws Exception {
-        return null;
+        Map<String, List<String>> splitItems = new HashMap<>();
+
+        LinkedList<String> remainingItems = new LinkedList<>(items);
+
+        while (remainingItems.size() > 0) {
+            int[] arrayCountingItemsHandledByEachDeliveryMethod = new int[methodsOfDeliveryMapping.size()];
+
+            for (String item : remainingItems) {
+                if (productsAndDeliveryMethods.containsKey(item)) {
+                    for (String deliveryMethod : productsAndDeliveryMethods.get(item)) {
+                        arrayCountingItemsHandledByEachDeliveryMethod[methodsOfDeliveryMapping.get(deliveryMethod)]++;
+                    }
+                } else {
+                    throw new ProductNotExistsException(String.format("Product %s not found.", item));
+                }
+            }
+
+            String optimalDeliveryMethod = methodsOfDeliveryMapping.getKey(getMaxValueIndex(arrayCountingItemsHandledByEachDeliveryMethod));
+            splitItems.put(optimalDeliveryMethod, new LinkedList<>());
+
+            Iterator<String> remainingItemsIterator = remainingItems.iterator();
+
+            while (remainingItemsIterator.hasNext()) {
+                String item = remainingItemsIterator.next();
+                if (productsAndDeliveryMethods.get(item).contains(optimalDeliveryMethod)) {
+                    remainingItemsIterator.remove();
+                    splitItems.get(optimalDeliveryMethod).add(item);
+                }
+            }
+        }
+        return splitItems;
     }
 
-
+    private int getMaxValueIndex(int[] array) {
+        int maxIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
 }
